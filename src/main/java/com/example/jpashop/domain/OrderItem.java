4c40d10fa3cac7d235c86f7be09575e931765766
kimsinok0714 <javaenjoy0714@gmail.com>
2025-09-28 12:19:46 +0900

    Update OrderItem.java

--- src/main/java/com/example/jpashop/domain/OrderItem.java
+++ src/main/java/com/example/jpashop/domain/OrderItem.java
@@ -14,9 +14,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "order_item")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class OrderItem {
 
     @Id
@@ -31,11 +32,19 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
-
+   
+    @Column(name = "order_price")
     private int orderPrice;
 
     private int count;
+    
+    // 연관 관게 편의 메소드
+    public void setItem(Item item) {
+        this.item = item;
+        this.item.getOrderItems().add(this);
+    }
 
+    
     // 비즈니스 로직
     // 주문 항목 생성  
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
@@ -53,6 +62,7 @@ public class OrderItem {
         this.getItem().addStock(this.count);
     }
 
+    // 총금액 계산
     public int getTotalPrice() {
         return this.orderPrice * this.count;
     }
